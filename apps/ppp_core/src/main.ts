c69b0c1ef0c03a8c96f24dafc8167f7ba0c6fee7
Aaron <aaalenortbless@gmail.com>
2025-11-05 10:57:32 -0500

    Fix: CreacionMicroservices

--- apps/ppp_core/src/main.ts
+++ apps/ppp_core/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { PppCoreModule } from './ppp_core.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(PppCoreModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    PppCoreModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3001
+      }
+    }
+  );
+  await app.listen();
 }
 bootstrap();
